07207b32ba41c5d053869848f85ae4e5eca8ceb0
kmnair <kmnair@ad3.ucdavis.edu@pc25.cs.ucdavis.edu>
2021-04-04 20:35:55 -0700

    first commit

---
+++ initial-utilities/wcat/wcat.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include <fcntl.h>
+#include <stdlib.h>
+ 
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+  // read file from user and print it's content
+  return 0;
+}
