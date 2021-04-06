926cd82412c96d0706b897d0e22a9d4dbb02cfc4
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-05 20:00:33 -0700

    wcat 4/5

--- initial-utilities/wcat/wcat.cpp
+++ initial-utilities/wcat/wcat.cpp
@@ -10,32 +10,36 @@
 using namespace std;
 
 int main (int argc, char *argv[]){
-  // init vars
-  int fd;
-  char buffer[4096];
-  int ret;
-  int bytesRead = 0;
-  
-  // read file from user and print it's content
-  
-  //check if file is open
-  if (argc == 1){
-    fd = STDIN_FILENO;
-  }
-  //if file is not open, open in read only
-  else{
-  fd = open(argv[1], O_RDONLY);
-  if (fd == -1){
-      cerr <<"wcat: cannot open file" << endl;
-      return 1;
-    }
-  }
-  while ((ret = read(fd,buffer, 4096)) > 0){
-    
-  }
+    // init vars
+    int fd;
+    char buffer[4096];
+    int ret;
+    int bytesRead = 0;
 
-  if (argc != 1){
-    close(fd);
-  }
-  return 0;
+    // read file from user and print it's content
+
+    //check if file is open
+    if (argc == 1){
+        fd = STDIN_FILENO;
+    }
+    //if file is not open, open in read only
+    else{
+        fd = open(argv[1], O_RDONLY);
+        if (fd == -1){
+            cerr <<"wcat: cannot open file" << endl;
+            return 1;
+        }
+    }
+    while ((ret = read(fd,buffer, 4096)) > 0){
+        ret = write(fd, buffer, bytesRead);
+    }
+
+    if (argc != 1){
+        close(fd);
+    }
+    else{
+        cerr<< "wcat: cannot close file" <<endl;
+    }
+
+    return 0;
 }
