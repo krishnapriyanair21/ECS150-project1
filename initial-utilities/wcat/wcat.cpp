a058c0d95b56397ff8a1f3d7fe6d0e3351088e4a
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-06 20:03:53 -0700

    wcat

--- initial-utilities/wcat/wcat.cpp
+++ initial-utilities/wcat/wcat.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
 #include <fcntl.h>
 #include <stdlib.h>
- 
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -11,32 +9,32 @@ using namespace std;
 
 int main (int argc, char *argv[]){
     // init vars
-    int fd;
+    int fd = 0;
     char buffer[4096];
-    int ret;
+    int ret = 0;
     int i = 0;
 
     if (argc == 1){
         return 0; // if no file, return 
     }
-    else{        
-        fd = open(argv[1], O_RDONLY);
-        i = 1;
+
+    for (int j = argc; j > 1; j--){
+        fd = open(argv[++i], O_RDONLY);
+        if (fd == -1){  /// exit if bad file
+            cerr <<"wcat: cannot open file "<<  endl;
+            return 1;
+        }
+        while ((ret = read(fd, buffer, 4096)) > 0 ){
+           ret = write(STDOUT_FILENO, buffer, ret);
+        }
     }
 
-    if (fd == -1){ 
-        cerr <<"wcat: cannot open file "<<  endl;
-        return 1;
+    while (fd >= 3){
+        if (close(fd) > 0 ){
+            cerr<< "wcat: cannot close file" <<endl;
+        }
+        else fd--;
     }
-
-    while ((ret = read(fd,buffer, 4096)) > 0){
-        ret = write(STDOUT_FILENO, buffer, ret);
-        /// need to loop through opening multiple files
-    }
-
-    if (close(fd) < 0){
-        cerr<< "wcat: cannot close file" <<endl;
-    }
-
+    
     return 0;
 }
