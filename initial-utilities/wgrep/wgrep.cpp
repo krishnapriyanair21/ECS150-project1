efdeb7e93dbd4119e2ff8e904ee6f9e41712ba6d
Krishnapriya Nair <krishnapriya.nair21@gmail.com>
2021-04-06 22:04:31 -0700

    completed wcat

--- initial-utilities/wgrep/wgrep.cpp
+++ initial-utilities/wgrep/wgrep.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-
 #include <fcntl.h>
 #include <stdlib.h>
- 
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
@@ -10,8 +8,37 @@
 using namespace std;
 
 int main (int argc, char *argv[]){
-  
-  // passed a search term
+    // init vars
+    int fd = 0;
+    char buffer[4096];
+    int ret = 0;
+    int i = 0;
+
+    if (argc == 1){
+        return 0; // if no file, return 
+    }
+
+    for (int j = argc; j > 1; j--){
+        fd = open(argv[++i], O_RDONLY);
+        if (fd == -1){  /// exit if bad file
+            cout <<"wcat: cannot open file"<<  endl;
+            return 1;
+        }
+        while ((ret = read(fd, buffer, 4096)) > 0 ){
+           ret = write(STDOUT_FILENO, buffer, ret);
+        }
+    }
+
+    while (fd >= 3){
+        if (close(fd) > 0 ){
+            cout<< "wcat: cannot close file" <<endl;
+            return 1;
+        }
+        else fd--;
+    }
+    return 0;
+
+     // passed a search term
   // search through each line and print line if search term is found
 
   // if no string is passed, print "wgrep: searchterm [file ...]"<<endl;
@@ -21,5 +48,4 @@ int main (int argc, char *argv[]){
 
   // if no file, read from STDIN
   // empty string = match with no lines
-  return 0;
 }
